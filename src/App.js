4980310bf2d06544b86d9050efdc354a40a4d677
HoanBaoNguyen <nguyenhoanbao4@gmail.com>
2024-06-25 20:58:53 +0700

    hello13

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name=Bao;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hoan bao 
-        </p>
+          hoan bao 12345
+        </p>      
       </header>
     </div>
   );
